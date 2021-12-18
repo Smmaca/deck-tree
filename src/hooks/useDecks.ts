29932d1684cd0e02f22fca9f3d278624a3d4f700
Shavaun MacArthur <shavaunmac@gmail.com>
2021-12-19 09:35:55 +1300

    Remove old deck

--- src/hooks/useDecks.ts
+++ src/hooks/useDecks.ts
@@ -1,9 +1,7 @@
 import { Deck } from "../dataTypes";
-import excuseDeck from '../decks/excuses.json';
 import iFeelLikeShitDeck from '../decks/iFeelLikeShit.json';
 
 const decks = [
-  excuseDeck,
   iFeelLikeShitDeck
 ];
 
